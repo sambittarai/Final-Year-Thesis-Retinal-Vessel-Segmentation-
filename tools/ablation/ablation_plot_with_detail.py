c622cdc4be61c09c6175cf8996d5e790216f5dd9
Sambit Tarai <44292855+sambittarai@users.noreply.github.com>
2021-05-05 10:25:03 +0530

    Update ablation_plot_with_detail.py

--- tools/ablation/ablation_plot_with_detail.py
+++ tools/ablation/ablation_plot_with_detail.py
@@ -8,7 +8,6 @@ from mpl_toolkits.axes_grid1.inset_locator import mark_inset
 from mpl_toolkits.axes_grid1.inset_locator import inset_axes
 import matplotlib.pylab as pylab
 import os
-from os.path import join
 from collections import OrderedDict
 import numpy as np
 
@@ -20,20 +19,20 @@ params = {'legend.fontsize': 12,
 pylab.rcParams.update(params)
 #Area under the ROC curve
 
-result_list = {"d_base":'./experiments/db1_new/result.npy',
-               "d_up1":'./experiments/d_up1/result.npy',
-               "d_total":'./experiments/d_total/result.npy'}
-save_path = './experiments/Drive_ablation'
+result_list = {"d_base":'/content/drive/MyDrive/Retinal_Vessel_Segmentation/Experiments/db1_new/result.npy',
+               "d_up1":'/content/drive/MyDrive/Retinal_Vessel_Segmentation/Experiments/d_up1/result.npy',
+               "d_total":'/content/drive/MyDrive/Retinal_Vessel_Segmentation/Experiments/d_total/result.npy'}
+save_path = '/content/drive/MyDrive/Retinal_Vessel_Segmentation/Experiments/Drive_ablation'
 
 if not os.path.exists(save_path): os.makedirs(save_path)
 
 # ===============AUC ROC===============
 fig, ax = plt.subplots(1, 1)
-axins = ax.inset_axes((0.45, 0.45, 0.45, 0.45)) # 左下角坐标（x0, y0）,窗口大小（ width, height）   
+axins = ax.inset_axes((0.45, 0.45, 0.45, 0.45))   
 axins.set_xlim(0.07, 0.11)
 axins.set_ylim(0.93, 0.96)
 
-mark_inset(ax, axins, loc1=3, loc2=1, fc="none", ec='k', lw=1) # 建立连线 loc1和loc2取值为1，2，3，4，对应右上，左上，左下，右下
+mark_inset(ax, axins, loc1=3, loc2=1, fc="none", ec='k', lw=1)
 for name,path in result_list.items():
     data = np.load(path[0])
     target, output = data
@@ -52,11 +51,11 @@ plt.savefig("{}/ROC1.png".format(save_path))
 
 # ===============AUC_PR==================
 fig, ax = plt.subplots(1, 1)
-axins = ax.inset_axes((0.15, 0.45, 0.4, 0.4)) # 左下角坐标（x0, y0）,窗口大小（ width, height）   
+axins = ax.inset_axes((0.15, 0.45, 0.4, 0.4))   
 axins.set_xlim(0.83,0.89)
 axins.set_ylim(0.75,0.82)
 
-mark_inset(ax, axins, loc1=1, loc2=4, fc="none", ec='k', lw=1) # 建立连线 loc1和loc2取值为1，2，3，4，对应右上，左上，左下，右下
+mark_inset(ax, axins, loc1=1, loc2=4, fc="none", ec='k', lw=1)
 for name,path in result_list.items():
     data = np.load(path[0])
     target, output = data
